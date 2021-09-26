20a2ff14d88029b380d14084040bbc2a56682b4c
qubetzl <qubetzl@protonmail.com>
2021-09-26 22:21:40 +0300

    Reworked build.sh script
    Reworked build.sh to be able to build from both official site and Github

--- build.sh
+++ build.sh
@@ -3,9 +3,18 @@
 set -e
 set -u
 
-curl -SsL -o looking-glass.tar.gz https://looking-glass.io/ci/host/source?id=stable
-tar -xvf looking-glass.tar.gz
-mv looking-glass-*/ looking-glass/
+url="https://looking-glass.io/ci/host/source?id=stable"
+
+#commit_hash="2160dee23abc8c0d09d7c6e742dab49ddbcb61fd"
+#url="https://github.com/gnif/LookingGlass/archive/${commit_hash}.tar.gz"
+
+
+mkdir /looking-glass
+cd /looking-glass
+
+curl -SsL -o looking-glass.tar.gz "${url}"
+tar --strip-components=1 -xvf looking-glass.tar.gz
+
 mkdir /looking-glass/client/build
 cd /looking-glass/client/build
 
